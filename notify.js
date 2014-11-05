14356acfa9a94b2f4b32644ae7cd000c039fa1e9
Hasnain Afzal <huss22@users.noreply.github.com>
2014-11-05 18:01:25 +1100

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,6 +1,5 @@
 	document.addEventListener('deviceready', function () {
-	    
-	var now                  = new Date().getTime(),
+		var now                  = new Date().getTime(),
 	    _30_seconds_from_now = new Date(now + 30*1000);
 
 	window.plugin.notification.local.add({
@@ -10,6 +9,15 @@
 	    repeat:  'weekly',
 	    date:    _30_seconds_from_now
 	});
+
+	window.plugin.notification.local.getScheduledIds(function (scheduledIds) {
+    alert('Scheduled IDs: ' + scheduledIds.join(' ,'));
+	}, scope);
+	
+	window.plugin.notification.local.add({ message: 'Great app!' });
+
 	}, false);
+	
+window.plugin.notification.local.add({ message: 'Great app Mate!' });
 
 

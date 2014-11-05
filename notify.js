ab4cc9c1535419854914035b7f5b77051bd7d9c8
Hasnain Afzal <huss22@users.noreply.github.com>
2014-11-06 10:23:34 +1100

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,17 +1,3 @@
-document.addEventListener('deviceready', function () {
-    alert("now available");
-}, false);
-
-function notifyme(){
-    
-    var now                  = new Date().getTime(),
-    _60_seconds_from_now = new Date(now + 60*1000);
-
-    window.plugin.notification.local.add({
-        id:      1,
-        title:   'Reminder',
-        message: 'Dont forget to be super smart and rich yo!',
-        repeat:  'weekly',
-        date:    _60_seconds_from_now
-    });
-}
+document.addEventListener("deviceready", function(){
+      alert("123");
+ },true);
